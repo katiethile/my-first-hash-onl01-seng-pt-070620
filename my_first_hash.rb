25a2b6ff832d388024108d4c583e0c89045dec0b
Katie Le <lethikatie@gmail.com>
2020-07-16 19:12:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,5 +30,5 @@ def adding
  # add 2 muskets to the shipping_manifest hash below
  # add 4 gun powder to the shipping_manifest hash below
 # return the shipping_manifest hash below
-shipping_manifest = {muskets
+shipping_manifest = {"muskets"] = 2 
 end
