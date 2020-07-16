936c097e18e130e81093db284b31ecfce72b387c
Katie Le <lethikatie@gmail.com>
2020-07-16 19:19:10 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -27,8 +27,8 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-    end
+                 
 shipping_manifest = ["muskets"] = 2 
 shipping_manifest = ["gun powder"] = 4
-end
 puts shipping_manifest
+end
